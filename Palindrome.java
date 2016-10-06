1164cd90003fdd9b3000c78ac9c27aa393551287
Nathan Faucett <nathanfaucett@gmail.com>
2016-10-06 09:45:01 -0500

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,31 @@
+import java.util.Stack;
+
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		String value = args[0];
+		System.out.println(value + " is" + (isPalindrome(args[0]) ? "" : " not") + " a palindrome");
+	}
+	
+	public static boolean isPalindrome(String word) {
+		Stack<Character> forward = new Stack<>();
+		Stack<Character> backword = new Stack<>();
+		
+		for (int i = 0; i < word.length(); i++) {
+			char ch = word.charAt(i);
+			
+			if (Character.isLetterOrDigit(ch)) {
+				forward.push(Character.toLowerCase(ch));
+			}
+		}
+		
+		Stack<Character> f = (Stack<Character>) forward.clone();
+		
+		while (!f.isEmpty()) {
+			backword.push(f.pop());
+		}
+		
+		return forward.equals(backword);
+	}
+}
